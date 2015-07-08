3b9191e575c05ca635d77ee07ad6fff8c1885fc0
Colin Shaw <colingshaw@yahoo.com>
2015-07-08 18:02:17 -0500

    First commit

---
+++ match.rb
@@ -0,0 +1,37 @@
+# A Ruby Complimentary Component Value Matcher
+# Copyright (c) 2015, Colin Shaw
+# Distributed under the terms of the MIT License
+
+class Match
+    
+    attr_reader :result
+
+    def report
+        percent = 200 * Float(@result.count) / Float(@total)
+        printf("%d pairs found from %d total parts (%.0f%% matched)\n",@result.count,@total,percent)
+        @result.each { |v| printf("   [ %.2f | %.2f ]   (%.2f)\n",v[0],v[1],v[2]) }
+    end
+
+    def match(threshold)
+        @result, matched = [], true
+        while @a.count>0 && @b.count>0 && matched==true do
+            d, matched = [], false
+            @a.each.with_index do |va,ia|
+                @b.each.with_index { |vb,ib| d << [(va-vb).abs,va,vb,ia,ib] }
+            end      
+            l = d.sort! { |x,y| x[0] <=> y[0] } [0]
+            if l[0] <= threshold
+                matched = true
+                @result << [l[1],l[2],l[0]]
+                @a.delete_at(l[3])
+                @b.delete_at(l[4])
+            end
+        end
+        self
+    end
+
+    def initialize(a,b)
+        @a, @b, @total = a, b, a.count+b.count
+    end
+
+end
---
+++ match_csv.rb
@@ -0,0 +1,17 @@
+# A Ruby Complimentary Component Value Matcher
+# Copyright (c) 2015, Colin Shaw
+# Distributed under the terms of the MIT License
+
+require 'csv'
+require './match.rb'
+
+filename  = ARGV[0]
+threshold = Float(ARGV[1])
+a, b      = [], []
+
+CSV.foreach(filename) do |r|
+    a << Float(r[0]) unless r[0].nil?
+    b << Float(r[1]) unless r[1].nil?
+end
+
+Match.new(a,b).match(threshold).report
